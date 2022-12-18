34eb0573c184b21822991b29ed051df90562077e
hieu34 <buitrunghieu987@gmail.com>
2022-12-18 09:49:12 +0700

    18.12

--- steps/sign-up/log-in-facebook.js
+++ steps/sign-up/log-in-facebook.js
@@ -8,15 +8,18 @@ const passFacebook = ["nguyenvanquanghuy"];
 
 const logInFacebook = async (page) => {
   const selectEmailFace = `[placeholder='Mobile number or email address']`;
-  await page.waitForSelector(selectEmailFace);
-  await page.focus(selectEmailFace);
-  await page.type(selectEmailFace, emailFace);
-
+  const waitEmailTypingAppear = await page.$(selectEmailFace);
+  if (waitEmailTypingAppear !== null) {
+    await page.focus(selectEmailFace);
+    await page.type(selectEmailFace, emailFace);
+  }
   const selectPassword = `[placeholder='Password']`;
-  await page.waitForSelector(selectPassword);
-  await page.focus(selectPassword);
-  await page.type(selectPassword, passFacebook);
-
+  const waitPasswordTypingAppear = await page.$(selectPassword);
+  if (waitPasswordTypingAppear !== null) {
+  // await page.waitForSelector(selectPassword);
+    await page.focus(selectPassword);
+    await page.type(selectPassword, passFacebook);
+  }
   const [button] = await page.$x("//button[contains(., 'Log In')]");
 
   if (button) {
@@ -28,11 +31,22 @@ const logInFacebook = async (page) => {
   if (buttonConfirm) {
     await buttonConfirm.click();
   }
+
+  await page.waitForNavigation();
+
+  // Scroll to the bottom of the page
+  await page.evaluate(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
+
+  // await page.waitForSelector('.class_name');
   // const page = await createIconigtoPage(context);
   // page.setViewport({ width: 1280, height: 720 });
   // await page.goto("https://facebook.com", { waitUntil: "networkidle2" });
 
   // return page;
+  //git
 };
 
 module.exports = logInFacebook;

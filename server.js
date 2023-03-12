56d4160b09bec74570ce80fbdabb49f2bfd666c2
hieu34 <buitrunghieu987@gmail.com>
2023-03-12 22:06:37 +0700

    12.03

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const openBrowser = require(".");
 const cors = require("cors");
+const {google} = require('googleapis');
+
+// Load credentials from JSON key file
+const creds = require('E://Workspace//Tool//facebook-adopt//credentialskey.json');
 
 const app = express();
 app.use(cors());
@@ -9,18 +13,65 @@ app.use(bodyParser.json());
 
 let scenarios = [];
 
-const steps = [
-  {
-    id: "a",
-    name: "Đăng nhập facebook",
-    functionName: "loginFacebook",
-  },
-  {
-    id: "b",
-    name: "Lướt newfeed",
-    functionName: "scrollFacebook",
-  },
-];
+const steps = [];
+
+// Create a new JWT client using the credentials
+const authClient = new google.auth.JWT(
+  creds.client_email,
+  null,
+  creds.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets']
+);
+
+// Authorize the client
+authClient.authorize(function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  // Create an instance of the Sheets API client with the authorized client
+  const sheets = google.sheets({ version: 'v4', auth: authClient });
+
+  // Read from scenario sheet
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1zlivcZ2xPrKVgO8l8Zq6iWhv21DWK9fnVPvgS6W3xgk',
+    range: 'Sheet1!A1:B2',
+  }, (err, res) => {
+    if (err) return console.log(`The API returned an error: ${err}`);
+    const rows = res.data.values;
+    console.log(`First two rows of data:`);
+    console.log(rows);
+  });
+
+  // Read from Steps sheet
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1zlivcZ2xPrKVgO8l8Zq6iWhv21DWK9fnVPvgS6W3xgk',
+    range: 'Steps!A1:C10',
+  }, (err, res) => {
+    if (err) return console.log(`The API returned an error: ${err}`);
+    const rows = res.data.values;
+    console.log(`First two rows of Steps data:`);
+    console.log(rows);
+  });
+
+  // Write to a sheet
+  sheets.spreadsheets.values.update({
+    spreadsheetId: '1zlivcZ2xPrKVgO8l8Zq6iWhv21DWK9fnVPvgS6W3xgk',
+    range: 'Sheet1!A1:B2',
+    valueInputOption: 'RAW',
+    resource: {
+      values: [
+        ['New value A1', 'New value B1'],
+        ['New value A2', 'New value B2'],
+      ],
+    },
+  }, (err, res) => {
+    if (err) return console.log(`The API returned an error: ${err}`);
+    console.log(`Updated ${res.data.updatedCells} cells.`);
+  });
+});
+
 app.get("/scenarios", (req, res) => {
   res.json({
     scenarios: scenarios,
@@ -28,8 +79,28 @@ app.get("/scenarios", (req, res) => {
 });
 
 app.get("/steps", (req, res) => {
-  res.json({
-    steps: steps,
+  const sheets = google.sheets({ version: 'v4', auth: authClient });
+
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1zlivcZ2xPrKVgO8l8Zq6iWhv21DWK9fnVPvgS6W3xgk',
+    range: 'Steps!A1:C', // Assuming the data starts at row 2 and has three columns (id, name, functionName)
+  }, (err, response) => {
+    if (err) return console.log(`The API returned an error: ${err}`);
+
+    const rows = response.data.values;
+    const newSteps = rows.map(row => {
+      return {
+        id: row[0],
+        name: row[1],
+        functionName: row[2]
+      };
+    });
+
+    steps.push(...newSteps); // Push the new steps to the existing array
+
+    res.json({
+      steps: steps,
+    });
   });
 });
 
@@ -87,3 +158,15 @@ app.post("/run-scenario", async (req, res) => {
 app.listen(3001, () => {
   console.log("Server listening on port 3001");
 });
+
+
+
+// const fs = require('fs').promises;
+// const path = require('path');
+// const process = require('process');
+// const {authenticate} = require('@google-cloud/local-auth');
+
+
+
+
+
